24740544f19d598e117e341bf76537e8af432af9
Md Moazz <moazzemh222@gmail.com>
2025-01-13 11:48:56 +0600

    Basic Controls

--- Basic Controls/Basic Controls/Basic Controls/obj/Debug/net8.0-windows/Basic Controls_ovhlm1u4_wpftmp.AssemblyInfo.cs
+++ Basic Controls/Basic Controls/Basic Controls/obj/Debug/net8.0-windows/Basic Controls_ovhlm1u4_wpftmp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("The TextBlock control")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Basic Controls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e073031bba645b10501eef25513f593405a801")]
-[assembly: System.Reflection.AssemblyProductAttribute("The TextBlock control")]
-[assembly: System.Reflection.AssemblyTitleAttribute("The TextBlock control")]
+[assembly: System.Reflection.AssemblyProductAttribute("Basic Controls")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Basic Controls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
