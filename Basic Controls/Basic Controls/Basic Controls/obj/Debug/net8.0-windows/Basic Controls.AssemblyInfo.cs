49e5c04d445477b7a609f0007867f18373935521
Md Moazz <moazzemh222@gmail.com>
2025-01-13 17:02:43 +0600

    Label Control

--- Basic Controls/Basic Controls/Basic Controls/obj/Debug/net8.0-windows/Basic Controls.AssemblyInfo.cs
+++ Basic Controls/Basic Controls/Basic Controls/obj/Debug/net8.0-windows/Basic Controls.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basic Controls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f386eeee0f0859b02407357c04eb46e00bfe7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b117752d38b6e544c003e8e99bba0583e721f8c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basic Controls")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basic Controls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Basic Controls/Basic Controls/The Label Control/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Basic Controls/Basic Controls/The Label Control/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace The_Label_Control {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/The Label Control;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/The Label Control;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
