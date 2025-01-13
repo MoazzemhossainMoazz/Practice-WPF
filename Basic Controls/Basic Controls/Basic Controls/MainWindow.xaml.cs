f1f386eeee0f0859b02407357c04eb46e00bfe7d
Md Moazz <moazzemh222@gmail.com>
2025-01-13 14:28:28 +0600

    TextBox control inline formatting

--- Basic Controls/Basic Controls/Basic Controls/MainWindow.xaml.cs
+++ Basic Controls/Basic Controls/Basic Controls/MainWindow.xaml.cs
@@ -19,6 +19,19 @@ namespace Basic_Controls
         public MainWindow()
         {
             InitializeComponent();
+            //TextBlock tb = new TextBlock();
+            //tb.TextWrapping = TextWrapping.Wrap;
+            //tb.Margin = new Thickness(40);
+            //tb.Inlines.Add("An example on");
+            //tb.Inlines.Add(new Run("the TextBlock control ") { FontWeight = FontWeights.Bold });
+            //tb.Inlines.Add("using ");
+            //tb.Inlines.Add(new Run("inline ") { FontStyle = FontStyles.Italic });
+            //tb.Inlines.Add(new Run("text formatting ") { Foreground = Brushes.Blue });
+            //tb.Inlines.Add("from ");
+            //tb.Inlines.Add(new Run("Code-Behind") { TextDecorations = TextDecorations.Underline });
+            //tb.Inlines.Add(".");
+            //this.Content = tb;
+
         }
     }
 }
