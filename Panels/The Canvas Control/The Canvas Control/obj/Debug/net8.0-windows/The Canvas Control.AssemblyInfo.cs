5b997eff753721bfaa921868613dc1794e1776ab
Md Moazz <moazzemh222@gmail.com>
2025-01-18 13:43:08 +0600

    Panels

--- Panels/The Canvas Control/The Canvas Control/obj/Debug/net8.0-windows/The Canvas Control.AssemblyInfo.cs
+++ Panels/The Canvas Control/The Canvas Control/obj/Debug/net8.0-windows/The Canvas Control.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Access Keys")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("The Canvas Control")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da270192846d39efa3c09f896d83c09fb3988e6")]
-[assembly: System.Reflection.AssemblyProductAttribute("Access Keys")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Access Keys")]
+[assembly: System.Reflection.AssemblyProductAttribute("The Canvas Control")]
+[assembly: System.Reflection.AssemblyTitleAttribute("The Canvas Control")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
